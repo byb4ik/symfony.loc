49612dbc8e83a6ad5726edca77192f3609d50075
Den <ferd9002@ukr.net>
2020-11-18 23:28:49 +0500

    good_lucky

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -96,8 +96,9 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('lucky'));
+        //return new RedirectResponse($this->urlGenerator->generate('/login'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
